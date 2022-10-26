688d9c529009b964ed0c22ef5883afa40913eb12
Miguel Muscat <miguelmuscat93@gmail.com>
2022-10-26 10:08:32 +0200

    Add a noop item processor

---
+++ src/Aggregator/NoopItemProcessor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RebelCode\Iris\Aggregator;
+
+use RebelCode\Iris\Data\Feed;
+use RebelCode\Iris\StoreQuery;
+
+/** An item processor that simply returns the list of items it was given, without making any changes. */
+class NoopItemProcessor implements ItemProcessor
+{
+    /** @inheritDoc */
+    public function process(array $items, Feed $feed, StoreQuery $query): array
+    {
+        return $items;
+    }
+}
---
+++ tests/Aggregator/NoopItemProcessorTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace RebelCode\Iris\Test\Func\Aggregator;
+
+use PHPUnit\Framework\TestCase;
+use RebelCode\Iris\Aggregator\ItemProcessor;
+use RebelCode\Iris\Aggregator\NoopItemProcessor;
+use RebelCode\Iris\Data\Feed;
+use RebelCode\Iris\Data\Item;
+use RebelCode\Iris\StoreQuery;
+
+class NoopItemProcessorTest extends TestCase
+{
+    public function testImplementsItemProcessorInterface()
+    {
+        $this->assertInstanceOf(ItemProcessor::class, new NoopItemProcessor());
+    }
+
+    public function testProcess()
+    {
+        $items = [
+            $this->createMock(Item::class),
+            $this->createMock(Item::class),
+            $this->createMock(Item::class),
+        ];
+
+        $feed = $this->createMock(Feed::class);
+        $query = $this->createMock(StoreQuery::class);
+
+        $processor = new NoopItemProcessor();
+
+        $this->assertSame($items, $processor->process($items, $feed, $query));
+    }
+}
