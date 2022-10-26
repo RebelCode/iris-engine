28f8f96d22bc26cccb3a44a9d973353e1432995d
Miguel Muscat <miguelmuscat93@gmail.com>
2022-10-26 15:53:42 +0200

    Add a simple Aggregator implementation

---
+++ src/Aggregator/SimpleAggregator.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RebelCode\Iris\Aggregator;
+
+use RebelCode\Iris\Aggregator;
+use RebelCode\Iris\Data\Feed;
+use RebelCode\Iris\ItemProcessor;
+use RebelCode\Iris\StoreQuery;
+
+class SimpleAggregator implements Aggregator
+{
+    /** @var bool */
+    protected $doManualPagination;
+
+    /**
+     * Constructor.
+     *
+     * @param bool $doManualPagination Whether pagination should be done manually by the engine. Use false if the store
+     *                                 supports pagination natively.
+     */
+    public function __construct(bool $doManualPagination = false)
+    {
+        $this->doManualPagination = $doManualPagination;
+    }
+
+    /** @inheritDoc */
+    public function getFeedQuery(Feed $feed, ?int $count = null, int $offset = 0): ?StoreQuery
+    {
+        $query = StoreQuery::forSources($feed->sources);
+
+        if ($this->doManualPagination) {
+            return $query;
+        } else {
+            return $query->withCount($count)->withOffset($offset);
+        }
+    }
+
+    /** @inheritDoc */
+    public function getPreProcessor(Feed $feed, StoreQuery $query): ?ItemProcessor
+    {
+        return null;
+    }
+
+    /** @inheritDoc */
+    public function getPostProcessor(Feed $feed, StoreQuery $query): ?ItemProcessor
+    {
+        return null;
+    }
+
+    /** @inheritDoc */
+    public function doManualPagination(Feed $feed, StoreQuery $query): bool
+    {
+        return $this->doManualPagination;
+    }
+}
---
+++ tests/Aggregator/SimpleAggregatorTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace RebelCode\Iris\Test\Func\Aggregator;
+
+use PHPUnit\Framework\TestCase;
+use RebelCode\Iris\Aggregator;
+use RebelCode\Iris\Aggregator\SimpleAggregator;
+use RebelCode\Iris\Data\Feed;
+use RebelCode\Iris\Data\Source;
+use RebelCode\Iris\StoreQuery;
+use RebelCode\Iris\StoreQuery\Expression;
+use RebelCode\Iris\StoreQuery\Field;
+
+class SimpleAggregatorTest extends TestCase
+{
+    public function testImplementsAggregator(): void
+    {
+        $this->assertInstanceOf(Aggregator::class, new SimpleAggregator());
+    }
+
+    public function provideManualPagination(): array
+    {
+        return [
+            'store pagination' => [false],
+            'manual pagination' => [true],
+        ];
+    }
+
+    /** @dataProvider provideManualPagination */
+    public function testDoManualPagination(bool $doManualPagination): void
+    {
+        $aggregator = new SimpleAggregator($doManualPagination);
+
+        $feed = $this->createMock(Feed::class);
+        $query = $this->createMock(StoreQuery::class);
+
+        $this->assertEquals($doManualPagination, $aggregator->doManualPagination($feed, $query));
+    }
+
+    public function testGetFeedQuery(): void
+    {
+        $feed = new Feed(1, [
+            new Source('foo', ''),
+            new Source('bar', ''),
+        ]);
+        $count = 10;
+        $offset = 5;
+
+        $expectedCriterion = Expression::forProp(Field::SOURCE, Expression::IN, ['foo', 'bar']);
+
+        $aggregator = new SimpleAggregator();
+        $query = $aggregator->getFeedQuery($feed, $count, $offset);
+
+        $this->assertEquals($expectedCriterion, $query->criterion);
+        $this->assertEquals($count, $query->count);
+        $this->assertEquals($offset, $query->offset);
+    }
+
+    public function testGetFeedQueryManualPagination(): void
+    {
+        $feed = new Feed(1, [
+            new Source('foo', ''),
+            new Source('bar', ''),
+        ]);
+        $count = 10;
+        $offset = 5;
+
+        $expectedCriterion = Expression::forProp(Field::SOURCE, Expression::IN, ['foo', 'bar']);
+
+        $aggregator = new SimpleAggregator(true);
+        $query = $aggregator->getFeedQuery($feed, $count, $offset);
+
+        $this->assertEquals($expectedCriterion, $query->criterion);
+        $this->assertNull($query->count);
+        $this->assertEquals(0, $query->offset);
+    }
+
+    /** @dataProvider provideManualPagination */
+    public function testGetProcessors(bool $doManualPagination): void
+    {
+        $feed = $this->createMock(Feed::class);
+        $query = $this->createMock(StoreQuery::class);
+
+        $aggregator = new SimpleAggregator($doManualPagination);
+
+        $this->assertNull($aggregator->getPreProcessor($feed, $query));
+        $this->assertNull($aggregator->getPostProcessor($feed, $query));
+    }
+}
